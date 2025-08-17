3e655c2b93074c67e1668902824057ff6f27ef83
Jardel Machado <dell_machado@yahoo.com.br>
2025-08-17 20:19:40 -0300

    feat: cria componente para renderizar uma transacao

---
+++ src/app/home/components/transaction-item/transaction-item.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Transaction } from 'src/app/shared/transaction/interfaces/transaction';
+import { TransactionValue } from "../transaction-value/transaction-value";
+
+@Component({
+  selector: 'app-transaction-item',
+  imports: [MatCardModule, MatButtonModule, TransactionValue],
+  templateUrl: './transaction-item.html',
+  styleUrl: './transaction-item.scss',
+})
+export class TransactionItem {
+  transaction = input.required<Transaction>();
+}
---
+++ src/app/home/components/transaction-value/transaction-value.ts
@@ -0,0 +1,20 @@
+import { Component, computed, input } from '@angular/core';
+import { TransactionType } from 'src/app/shared/transaction/enums/transaction-type';
+import { Transaction } from 'src/app/shared/transaction/interfaces/transaction';
+
+const CSS_CLASSES = {
+  [TransactionType.INCOME]: 'income',
+  [TransactionType.OUTCOME]: 'outcome',
+}
+@Component({
+  selector: 'app-transaction-value',
+  imports: [],
+  templateUrl: './transaction-value.html',
+  styleUrl: './transaction-value.scss',
+  host: { '[class]': 'cssClass()' }
+})
+export class TransactionValue {
+  transaction = input.required<Transaction>();
+
+  cssClass = computed(() => CSS_CLASSES[this.transaction().type])
+}
