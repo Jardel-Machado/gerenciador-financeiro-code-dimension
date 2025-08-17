df233e22eacfbdfd221a901ca288270ebc68b4b3
Jardel Machado <dell_machado@yahoo.com.br>
2025-08-17 18:30:53 -0300

    feat: utiliza balance-card na home.

--- src/app/home/pages/home/home.ts
+++ src/app/home/pages/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { BalanceCard } from 'src/app/home/components/balance-card/balance-card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [BalanceCard],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
-export class Home {
-
-}
+export class Home {}
