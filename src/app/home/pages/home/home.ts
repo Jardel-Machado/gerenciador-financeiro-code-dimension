8954fc600b9fc182acf90a90fd79ac317ce31904
Jardel Machado <dell_machado@yahoo.com.br>
2025-08-17 20:20:31 -0300

    feat: renderiza uma lista de transações

--- src/app/home/pages/home/home.ts
+++ src/app/home/pages/home/home.ts
@@ -1,17 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { Balance } from 'src/app/home/components/balance/balance';
+import { Transaction } from 'src/app/shared/transaction/interfaces/transaction';
+import { TransactionItem } from '../../components/transaction-item/transaction-item';
+import { TransactionType } from 'src/app/shared/transaction/enums/transaction-type';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance],
+  imports: [Balance, TransactionItem],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  transactions = signal([
-    { value: 100, type: 'income' },
-    { value: 50, type: 'income' },
-    { value: 50, type: 'outcome' },
-    { value: 100, type: 'outcome' },
-  ])
+  transactions = signal<Transaction[]>([
+    { title: 'Salário', value: 100, type: TransactionType.INCOME },
+    { title: 'Freelance', value: 50, type: TransactionType.INCOME },
+    { title: 'Aluguel', value: 50, type: TransactionType.OUTCOME },
+    { title: 'Conta de Luz', value: 100, type: TransactionType.OUTCOME },
+  ]);
 }
