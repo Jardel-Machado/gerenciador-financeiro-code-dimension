2f9246a16ce2f9f62c88590b8e4182b2210e8d2e
Jardel Machado <dell_machado@yahoo.com.br>
2025-08-17 21:53:45 -0300

    feat: recupera dados usando TransactionService

--- src/app/home/pages/home/home.ts
+++ src/app/home/pages/home/home.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { Balance } from 'src/app/home/components/balance/balance';
-import { Transaction } from 'src/app/shared/transaction/interfaces/transaction';
 import { TransactionItem } from '../../components/transaction-item/transaction-item';
 import { NoTransactions } from 'src/app/home/components/no-transactions/no-transactions';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { TransactionService } from 'src/app/shared/transaction/services/transaction';
+import { Transaction } from 'src/app/shared/transaction/interfaces/transaction';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +12,27 @@ import { NoTransactions } from 'src/app/home/components/no-transactions/no-trans
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
-  transactions = signal<Transaction[]>([
+export class Home implements OnInit {
+  transactions = signal<Transaction[]>([]);
 
-  ]);
+  private readonly transactionService = inject(TransactionService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.getTransactions();
+  }
+
+  getTransactions() {
+    this.transactionService
+      .getAll()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (response) => {
+          this.transactions.set(response);
+        },
+        error: (error) => {
+          console.error('Error fetching transactions:', error);
+        },
+      });
+  }
 }
