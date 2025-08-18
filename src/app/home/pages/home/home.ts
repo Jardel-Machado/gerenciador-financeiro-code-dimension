3b4ffb6f97adfbd1d4ac44b9305cf67237cac0b0
Jardel Machado <dell_machado@yahoo.com.br>
2025-08-17 21:19:33 -0300

    chore: instala json-server

--- src/app/home/pages/home/home.ts
+++ src/app/home/pages/home/home.ts
@@ -2,7 +2,6 @@ import { Component, signal } from '@angular/core';
 import { Balance } from 'src/app/home/components/balance/balance';
 import { Transaction } from 'src/app/shared/transaction/interfaces/transaction';
 import { TransactionItem } from '../../components/transaction-item/transaction-item';
-import { TransactionType } from 'src/app/shared/transaction/enums/transaction-type';
 import { NoTransactions } from 'src/app/home/components/no-transactions/no-transactions';
 
 @Component({
@@ -13,9 +12,6 @@ import { NoTransactions } from 'src/app/home/components/no-transactions/no-trans
 })
 export class Home {
   transactions = signal<Transaction[]>([
-    { title: 'Salário', value: 100, type: TransactionType.INCOME },
-    { title: 'Freelance', value: 50, type: TransactionType.INCOME },
-    { title: 'Aluguel', value: 50, type: TransactionType.OUTCOME },
-    { title: 'Conta de Luz', value: 100, type: TransactionType.OUTCOME },
+
   ]);
 }
