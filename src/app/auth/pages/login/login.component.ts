89832f35eab8d13323c3c365d5865ca7c478a47f
Jardel-Machado <dell_machado@yahoo.com.br>
2025-10-04 22:41:09 -0300

    feat: adiciona autenticação

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { UserCredentials } from 'src/app/auth/interfaces/user-credentials';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +16,9 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class LoginComponent implements OnInit {
   form!: FormGroup;
+
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
   ngOnInit() {
     this.iniciarFormulario();
@@ -25,6 +32,27 @@ export class LoginComponent implements OnInit {
   }
 
   submit() {
-    console.log('submit', this.form.value);
+    if (this.form.invalid) {
+      return;
+    }
+
+    const payload: UserCredentials = {
+      user: this.form.value.user,
+      password: this.form.value.password
+    }
+
+    this.authService.login(payload).subscribe({
+      next: (response) => {
+        console.log('Login successful', response);
+        this.router.navigate(['']);
+      },
+      error: (error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.form.setErrors({
+            wrongCredentials: true
+          });
+        }
+      }
+    });
   }
 }
